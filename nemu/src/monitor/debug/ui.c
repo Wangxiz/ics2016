2c5702e2a4e01a2e17f956a10f55be5447206a7f
tracer-ics2016 <tracer@njuics.org>
2017-03-27 13:25:59 +0800

    >  compile NEMU 141220105 wangxiz Linux wangxiz-virtual-machine 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:28 UTC 2016 i686 i686 i686 GNU/Linux  13:25:59 up  1:23,  1 user,  load average: 0.10, 0.09, 0.07 37d8c61ddd96fc0697bab87bf45a2809800cd83

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -54,6 +54,16 @@ static int cmd_si(char *args) {
 }
 
 static int cmd_info(char *args) {
+	char *arg = strtok(NULL, " ");
+	if(strcmp(arg, "r") == 0) {
+		printf("eax\t%x\t%d", cpu.gpr[R_EAX]._32, cpu.gpr[R_EAX]._32);
+	}
+	else if(strcmp(arg, "w") == 0) {
+	
+	}
+	else {
+	
+	}
 	return -1;
 }
 
