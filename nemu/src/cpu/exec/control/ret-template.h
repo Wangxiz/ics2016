e641e0f222485f9d5165ff3617b2e93fc4e952cd
tracer-ics2016 <tracer@njuics.org>
2017-03-29 19:44:29 +0800

    >  compile NEMU 141220105 wangxiz Linux wangxiz-virtual-machine 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:28 UTC 2016 i686 i686 i686 GNU/Linux  19:44:29 up  5:05,  1 user,  load average: 0.18, 0.09, 0.09 5907b4bbca14e17297c361ac946ce36b21dca1f2

--- nemu/src/cpu/exec/control/ret-template.h
+++ nemu/src/cpu/exec/control/ret-template.h
@@ -3,7 +3,16 @@
 #define instr ret
 
 static void do_execute() {
-	
+	uint32_t addr = MEM_R(cpu.esp);
+	cpu.esp += 4 + op_src->val;
+	cpu.eip = addr;
+	if(op_src->val == 0) {
+		print_asm("ret");
+	}
+	else {
+		cpu.eip -= 2;
+		print_asm_template1();
+	}
 }
 
 make_instr_helper(n)
