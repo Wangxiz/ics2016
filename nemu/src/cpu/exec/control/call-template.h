b3d9158a616e2c4d581bb66fcfe0d98978e46997
tracer-ics2016 <tracer@njuics.org>
2017-03-29 19:28:19 +0800

    >  compile NEMU 141220105 wangxiz Linux wangxiz-virtual-machine 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:28 UTC 2016 i686 i686 i686 GNU/Linux  19:28:19 up  4:49,  1 user,  load average: 0.16, 0.20, 0.14 3d8bd5daffb415918a870dfbbfa564f5292dbbe8

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -5,7 +5,7 @@
 static void do_execute() {
 	cpu.esp -= DATA_BYTE;
 	MEM_W(cpu.esp, cpu.eip + DATA_BYTE + 1);
-	printf("ESP:%x\n", cpu.esp);
+	printf("ESP:%x\n", MEM_R(cpu.esp));
 	if(DATA_BYTE == 4) {
 		cpu.eip += op_src->val;
 	}
