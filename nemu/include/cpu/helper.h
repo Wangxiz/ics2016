d5db8245f4916ec673409dd21cb5eeb76ef00258
tracer-ics2016 <tracer@njuics.org>
2017-03-30 12:27:26 +0800

    >  compile NEMU 141220105 wangxiz Linux wangxiz-virtual-machine 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:28 UTC 2016 i686 i686 i686 GNU/Linux  12:27:26 up 10:30,  1 user,  load average: 0.37, 0.23, 0.18 a8cfb8df67cf2b025c27d8924b167c91f052053e

--- nemu/include/cpu/helper.h
+++ nemu/include/cpu/helper.h
@@ -17,13 +17,13 @@ int len;
 /* Used in cpu/exec/helper.h: make_instr_helper */
 static inline int idex(swaddr_t eip, int (*decode)(swaddr_t), void (*execute) (void)) {
 	/* eip is pointing to the opcode */
-
-	// printf("cpu/helper.EIP: 0x%08x\n", cpu.eip);
-
 	/* len: the length of an instruction except opcode */
 	len = decode(eip + 1);
-	Log("len:%d\n", len);
-	// printf("len:%d\n", len);
+
+#ifdef DEBUG_MY
+	Log("cpu/helper.EIP: 0x%08x, len:%d\n", cpu.eip, len);
+#endif
+
 	execute();
 	return len + 1;	// "1" for opcode
 }
