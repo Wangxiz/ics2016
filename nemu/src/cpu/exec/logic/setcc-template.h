66241a5f70f3514921052181f9ce6902b11cb5e2
tracer-ics2016 <tracer@njuics.org>
2017-03-30 19:21:23 +0800

    >  compile NEMU 141220105 wangxiz Linux wangxiz-virtual-machine 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:28 UTC 2016 i686 i686 i686 GNU/Linux  19:21:23 up 17:08,  1 user,  load average: 0.35, 0.15, 0.10 bf84cfbc8a4f449fc202259f4c480d92a950e8fb

--- nemu/src/cpu/exec/logic/setcc-template.h
+++ nemu/src/cpu/exec/logic/setcc-template.h
@@ -12,8 +12,98 @@ static void do_execute() { \
 } \
 make_instr_helper(rm) 
 
-#define instr setne
+#define instr setne 	/* also setnz */
 #define SETFLAG cpu.ZF == 0
+all_exec();
+#undef SETFLAG
+#undef instr
+
+#define instr setnbe	/* also seta */
+#define SETFLAG cpu.ZF == 0 && cpu.CF == 0
+all_exec();
+#undef SETFLAG
+#undef instr
+
+#define instr setae		/* also setnb | setnc */
+#define SETFLAG cpu.CF == 0
+all_exec();
+#undef SETFLAG
+#undef instr
+
+#define instr setb 		/* also setc | setnae */
+#define SETFLAG cpu.CF == 1
+all_exec();
+#undef SETFLAG
+#undef instr
+
+#define instr setbe 	/* also setna */
+#define SETFLAG cpu.CF == 1 || cpu.ZF == 1
+all_exec();
+#undef SETFLAG
+#undef instr
+
+#define instr sete 		/* also setz */
+#define SETFLAG cpu.ZF == 1
+all_exec();
+#undef SETFLAG
+#undef instr
+
+#define instr setg 	/* also setnle */
+#define SETFLAG cpu.ZF == 0 && cpu.SF == cpu.OF
+all_exec();
+#undef SETFLAG
+#undef instr
+
+#define instr setge 	/* also setnl */
+#define SETFLAG cpu.SF == cpu.OF
+all_exec();
+#undef SETFLAG
+#undef instr
+
+#define instr setl 		/* also setnge */
+#define SETFLAG cpu.SF != cpu.OF
+all_exec();
+#undef SETFLAG
+#undef instr
+
+#define instr setle 	/* also setng */
+#define SETFLAG cpu.ZF == 1 || cpu.SF != cpu.OF
+all_exec();
+#undef SETFLAG
+#undef instr
+
+#define instr setno
+#define SETFLAG cpu.OF == 0
+all_exec();
+#undef SETFLAG
+#undef instr
+
+#define instr setnp 	/* also setpo */
+#define SETFLAG cpu.PF == 0
+all_exec();
+#undef SETFLAG
+#undef instr
+
+#define instr setns
+#define SETFLAG cpu.SF == 0
+all_exec();
+#undef SETFLAG
+#undef instr
+
+#define instr seto
+#define SETFLAG cpu.OF == 1
+all_exec();
+#undef SETFLAG
+#undef instr
+
+#define instr setp 		/* also setpe */
+#define SETFLAG cpu.PF == 1
+all_exec();
+#undef SETFLAG
+#undef instr
+
+#define instr sets
+#define SETFLAG cpu.SF == 1
 all_exec();
 #undef SETFLAG
 #undef instr
--- nemu/src/cpu/exec/logic/setcc.h
+++ nemu/src/cpu/exec/logic/setcc.h
@@ -1,6 +1,21 @@
 #ifndef __SETCC_H__
 #define __SETCC_H__
 
-make_helper(setne_rm_b);
+make_helper(setne_rm_b);	/* 0x95 ok */
+make_helper(setnbe_rm_b);	/* 0x97 ok */
+make_helper(setae_rm_b);	/* 0x93 ok */
+make_helper(setb_rm_b);		/* 0x92 ok */
+make_helper(setbe_rm_b);	/* 0x96 ok */
+make_helper(sete_rm_b);		/* 0x94 ok */
+make_helper(setg_rm_b);		/* 0x9F ok */
+make_helper(setge_rm_b);	/* 0x9D ok */
+make_helper(setl_rm_b);		/* 0x9C ok */
+make_helper(setle_rm_b);	/* 0x9E ok */
+make_helper(setno_rm_b);	/* 0x91 ok */
+make_helper(setnp_rm_b);	/* 0x9B ok */
+make_helper(setns_rm_b);	/* 0x99 ok */
+make_helper(seto_rm_b);		/* 0x90 ok */
+make_helper(setp_rm_b);		/* 0x9A ok */
+make_helper(sets_rm_b);		/* 0x98 ok */
 
 #endif
