63da479499d61c4e3cfd909d214a602a1e5ac756
tracer-ics2016 <tracer@njuics.org>
2017-03-30 12:38:54 +0800

    >  compile NEMU 141220105 wangxiz Linux wangxiz-virtual-machine 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:28 UTC 2016 i686 i686 i686 GNU/Linux  12:38:54 up 10:42,  1 user,  load average: 0.16, 0.17, 0.17 2589604ba401b7190e35aa46cdd781484974128

---
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -0,0 +1,22 @@
+#include "cpu/exec/template-start.h"
+
+#define instr sub
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+static void do_execute () {
+	DATA_TYPE val = op_dest->val - op_src->val;
+	OPERAND_W(op_dest, val);
+
+	UPDATE_EFLAGS_ZF(val);
+	UPDATE_EFLAGS_SF(val);
+	UPDATE_EFLAGS_PF(val);
+	SUB_CF(op_dest->val, op_src->val);
+	SUB_OF(op_dest->val, op_src->val, val);
+
+	print_asm_template2();
+}
+
+make_instr_helper(si2rm)
+#endif
+
+#include "cpu/exec/template-end.h"
