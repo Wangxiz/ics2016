2e4c5dd0a4a5de3ff27316462c4fd058e6e2c8f8
tracer-ics2016 <tracer@njuics.org>
2017-03-31 20:08:14 +0800

    >  compile NEMU 141220105 wangxiz Linux wangxiz-virtual-machine 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:28 UTC 2016 i686 i686 i686 GNU/Linux  20:08:14 up 1 day,  4:27,  1 user,  load average: 0.10, 0.16, 0.10 235dac05cc08120dcf2a7e5e8a18ff3843589e3b

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -6,26 +6,27 @@ static void do_execute() {
 	op_src->type = op_dest->type = OP_TYPE_REG;
 	op_src->reg = R_ESI;
 
-	snprintf(op_src->str, 11, "%%ds:(%%esi)");
+	snprintf(op_src->str, 13, "%%ds:(%%esi),");
 	DATA_TYPE val = swaddr_read(cpu.esi, DATA_BYTE);
 
 	if(DATA_BYTE == 1) {
 		op_dest->reg = R_AL;
 		swaddr_write(cpu.gpr[0]._8[0], DATA_BYTE, val);
-		snprintf(op_src->str, 14, "%%ds:(%%esi),%%al");
+		snprintf(op_dest->str, 4, "%%al");
 	}
 	else if(DATA_BYTE == 2) {
 		op_dest->reg = R_AX;
 		swaddr_write(cpu.gpr[0]._16, DATA_BYTE, val);
-		snprintf(op_src->str, 14, "%%ds:(%%esi),%%ax");
+		snprintf(op_dest->str, 4, "%%ax");
 	}
 	else {
 		op_dest->reg = R_EAX;
 		swaddr_write(cpu.gpr[0]._32, DATA_BYTE, val);
-		snprintf(op_src->str, 15, "%%ds:(%%esi),%%eax");			
+		snprintf(op_dest->str, 5, "%%eax");			
 	}
 
 	cpu.esi += ((cpu.DF == 0) ? +DATA_BYTE : -DATA_BYTE);
+	print_asm_template2();
 }
 
 make_instr_helper(n)
