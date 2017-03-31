d68b67310c29b7ceec87d0a33d5b73dba4b05c99
tracer-ics2016 <tracer@njuics.org>
2017-03-31 18:56:54 +0800

    >  compile NEMU 141220105 wangxiz Linux wangxiz-virtual-machine 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:28 UTC 2016 i686 i686 i686 GNU/Linux  18:56:54 up 1 day,  3:16,  1 user,  load average: 0.27, 0.17, 0.11 e4ef6c0da8c18496e35c505e2496c5a90cb0453

--- nemu/src/cpu/exec/arith/add.h
+++ nemu/src/cpu/exec/arith/add.h
@@ -4,10 +4,12 @@
 make_helper(add_r2rm_b);	/* 0x00 ok */
 make_helper(add_rm2r_b);	/* 0x02 ok */
 make_helper(add_i2a_b);		/* 0x04 ok */
+make_helper(add_i2rm_b);	/* 0x80 /0 ok */
 
 make_helper(add_si2rm_v);	/* 0x83 /0 ok */
 make_helper(add_r2rm_v);	/* 0x01 ok */
 make_helper(add_rm2r_v);	/* 0x03 ok */
 make_helper(add_i2a_v);		/* 0x05 ok */
+make_helper(add_i2rm_v);	/* 0x81 /0 ok */
 
 #endif
