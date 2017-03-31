73543601d4068303262a024a4ee807342137ef8b
tracer-ics2016 <tracer@njuics.org>
2017-03-31 15:41:40 +0800

    >  compile NEMU 141220105 wangxiz Linux wangxiz-virtual-machine 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:28 UTC 2016 i686 i686 i686 GNU/Linux  15:41:40 up 1 day, 0 min,  1 user,  load average: 0.45, 0.18, 0.15 8d6d4b32a85a9cc2f12f1c59c9c998143a05ffc

--- nemu/src/cpu/exec/logic/or.h
+++ nemu/src/cpu/exec/logic/or.h
@@ -1,15 +1,15 @@
 #ifndef __OR_H__
 #define __OR_H__
 
-make_helper(or_i2a_b);
-make_helper(or_i2rm_b);
-make_helper(or_r2rm_b);
-make_helper(or_rm2r_b);
+make_helper(or_i2a_b);		/* 0x0c ok */
+make_helper(or_i2rm_b);		/* 0x80 /1 ok */
+make_helper(or_r2rm_b);		/* 0x08 ok */
+make_helper(or_rm2r_b);		/* 0x0a ok */
 
-make_helper(or_i2a_v);
-make_helper(or_i2rm_v);
-make_helper(or_si2rm_v);
-make_helper(or_r2rm_v);
-make_helper(or_rm2r_v);
+make_helper(or_i2a_v);		/* 0x0d ok */
+make_helper(or_i2rm_v);		/* 0x81 /1 ok */
+make_helper(or_si2rm_v);	/* 0x83 /1 ok */
+make_helper(or_r2rm_v);		/* 0x09 ok */
+make_helper(or_rm2r_v);		/* 0x0b ok */
 
 #endif
