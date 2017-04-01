b01d83d1d4f167eb6559f1829ce9208b7cf690b8
tracer-ics2016 <tracer@njuics.org>
2017-04-01 09:11:13 +0800

    >  compile NEMU 141220105 wangxiz Linux wangxiz-virtual-machine 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:28 UTC 2016 i686 i686 i686 GNU/Linux  09:11:13 up 1 day,  7:47,  1 user,  load average: 0.49, 0.43, 0.51 e47ac7878fece6d427c36ce4e8728cfa9da7e419

--- nemu/src/cpu/exec/arith/sbb-template.h
+++ nemu/src/cpu/exec/arith/sbb-template.h
@@ -11,7 +11,7 @@ static void do_execute () {
 	UPDATE_EFLAGS_ZF(val);
 	UPDATE_EFLAGS_SF(val);
 	UPDATE_EFLAGS_PF(val);
-	SUB_CF(op_dest->val, (op_src->val));
+	SUB_CF(op_dest->val, op_src->val);
 	SUB_OF(op_dest->val, op_src->val, val);
 
 	print_asm_template2();
