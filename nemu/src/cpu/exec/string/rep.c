bb915b205325129d503e37153c364c4ab239fa6c
tracer-ics2016 <tracer@njuics.org>
2017-04-01 13:04:39 +0800

    >  compile NEMU 141220105 wangxiz Linux wangxiz-virtual-machine 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:28 UTC 2016 i686 i686 i686 GNU/Linux  13:04:39 up 1 day,  8:56,  1 user,  load average: 0.42, 0.28, 0.18 8f5d193475070fc78c6a640d48f9becba680748

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -27,6 +27,11 @@ make_helper(rep) {
 				);
 
 			/* TODO: Jump out of the while loop if necessary. */
+			if((ops_decoded.opcode == 0xa6
+			 || ops_decoded.opcode == 0xa7
+			 || ops_decoded.opcode == 0xae
+			 || ops_decoded.opcode == 0xaf) && cpu.ZF == 0)
+				break;
 			// Log("EIP: 0x%08x\n", eip);
 		}
 		len = 1;
@@ -54,7 +59,11 @@ make_helper(repnz) {
 			  );
 
 		/* TODO: Jump out of the while loop if necessary. */
-
+		if((ops_decoded.opcode == 0xa6
+		 || ops_decoded.opcode == 0xa7
+		 || ops_decoded.opcode == 0xae
+		 || ops_decoded.opcode == 0xaf) && cpu.ZF == 0)
+			break;
 	}
 
 #ifdef DEBUG
