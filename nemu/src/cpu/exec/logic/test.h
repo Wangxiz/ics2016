ce2a631150f4c94418b8e2c2dcb556583ee0f180
tracer-ics2016 <tracer@njuics.org>
2017-04-01 14:54:43 +0800

    >  compile NEMU 141220105 wangxiz Linux wangxiz-virtual-machine 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:28 UTC 2016 i686 i686 i686 GNU/Linux  14:54:42 up 1 day, 10:08,  1 user,  load average: 0.26, 0.53, 1.14 d02a0a0b26701b07a5f8b993d2fd0bc6abe3ffc6

--- nemu/src/cpu/exec/logic/test.h
+++ nemu/src/cpu/exec/logic/test.h
@@ -1,8 +1,12 @@
 #ifndef __TEST_H__
 #define __TEST_H__
 
-make_helper(test_r2rm_b);
+make_helper(test_r2rm_b);	/* 0x84 ok */
+make_helper(test_i2rm_b);	/* 0xF6 /0 ok */
+make_helper(test_i2a_b);	/* 0xA8 ok */
 
-make_helper(test_r2rm_v);
+make_helper(test_r2rm_v);	/* 0x85 ok */
+make_helper(test_i2rm_v);	/* 0xF7 /0 ok */
+make_helper(test_i2a_v);	/* 0xA9 ok */
 
 #endif
