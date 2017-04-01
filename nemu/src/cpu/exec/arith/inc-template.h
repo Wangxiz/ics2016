af20df48e07f3488dd61a93af593e40baeca02a5
tracer-ics2016 <tracer@njuics.org>
2017-04-01 13:30:41 +0800

    >  compile NEMU 141220105 wangxiz Linux wangxiz-virtual-machine 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:28 UTC 2016 i686 i686 i686 GNU/Linux  13:30:41 up 1 day,  9:22,  1 user,  load average: 0.04, 0.14, 0.16 104f96666d653ec6f70938031abae61a1b43980a

--- nemu/src/cpu/exec/arith/inc-template.h
+++ nemu/src/cpu/exec/arith/inc-template.h
@@ -3,11 +3,17 @@
 #define instr inc
 
 static void do_execute () {
-	DATA_TYPE result = op_src->val + 1;
-	OPERAND_W(op_src, result);
+	DATA_TYPE val = op_src->val + 1;
+	OPERAND_W(op_src, val);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	// panic("please implement me");
+
+	UPDATE_EFLAGS_ZF(val);
+	UPDATE_EFLAGS_SF(val);
+	UPDATE_EFLAGS_PF(val);
+
+	ADD_OF(op_src->val, 1, val);
 
 	print_asm_template1();
 }
--- nemu/src/cpu/exec/arith/inc.h
+++ nemu/src/cpu/exec/arith/inc.h
@@ -1,9 +1,9 @@
 #ifndef __INC_H__
 #define __INC_H__
 
-make_helper(inc_rm_b);
+make_helper(inc_rm_b);	/* 0xFE /0 */
 
-make_helper(inc_rm_v);
-make_helper(inc_r_v);
+make_helper(inc_rm_v);	/* 0xFF /0 */
+make_helper(inc_r_v);	/* 0x40-0x47 */
 
 #endif
