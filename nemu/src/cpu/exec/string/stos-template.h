a33312168304645af6d3bf51753ca003e2125ba1
tracer-ics2016 <tracer@njuics.org>
2017-04-01 12:49:04 +0800

    >  compile NEMU 141220105 wangxiz Linux wangxiz-virtual-machine 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:28 UTC 2016 i686 i686 i686 GNU/Linux  12:49:04 up 1 day,  8:40,  1 user,  load average: 0.12, 0.15, 0.16 59654458d353ef4c5444e01694d2e047b8a8f94

--- nemu/src/cpu/exec/string/stos-template.h
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -9,21 +9,25 @@ static void do_execute() {
 	snprintf(op_dest->str, 11, "%%es:(%%edi)");
 
 	if (DATA_BYTE == 1) {
-		op_src->reg = R_AL; 
-		cpu.edi = REG(R_AL);
+		op_src->reg = R_AL;
+		MEM_W(cpu.edi, REG(R_AL));
+		// swaddr_write();
+		// cpu.edi = REG(R_AL);
 		snprintf(op_src->str, 4, "%%al");
 	}
 	else if(DATA_BYTE == 2) {
 		op_src->reg = R_AX; 
-		cpu.edi = REG(R_AX);
+		MEM_W(cpu.edi, REG(R_AX));
+		// cpu.edi = REG(R_AX);
 		snprintf(op_src->str, 4, "%%ax");
 	}
 	else {
 		op_src->reg = R_EAX; 
-		cpu.edi = REG(R_EAX);
+		MEM_W(cpu.edi, REG(R_EAX));
+		// cpu.edi = REG(R_EAX);
 		snprintf(op_src->str, 5, "%%eax");
 	}
-	
+
 	cpu.edi += ((cpu.DF == 0) ? +DATA_BYTE : -DATA_BYTE);
 
 	print_asm_template2();
