1b886c319f093698be1438dfa685e769748f59ff
tracer-ics2016 <tracer@njuics.org>
2017-04-01 14:59:56 +0800

    >  compile NEMU 141220105 wangxiz Linux wangxiz-virtual-machine 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:28 UTC 2016 i686 i686 i686 GNU/Linux  14:59:56 up 1 day, 10:14,  1 user,  load average: 0.26, 0.33, 0.87 a11e528924dde00766ff893f1a4a87cee23a810c

--- nemu/src/cpu/exec/logic/sar.h
+++ nemu/src/cpu/exec/logic/sar.h
@@ -1,11 +1,11 @@
 #ifndef __SAR_H__
 #define __SAR_H__
 
-make_helper(sar_rm_1_b);
-make_helper(sar_rm_cl_b);
-make_helper(sar_rm_imm_b);
+make_helper(sar_rm_1_b);	/* 0xD0 /7 ok */
+make_helper(sar_rm_cl_b);	/* 0xD2 /7 ok */
+make_helper(sar_rm_imm_b);	/* 0xC0 /7 ok */
 
-make_helper(sar_rm_1_v);
-make_helper(sar_rm_cl_v);
-make_helper(sar_rm_imm_v);
+make_helper(sar_rm_1_v);	/* 0xD1 /7 ok */
+make_helper(sar_rm_cl_v);	/* 0xD3 /7 ok */
+make_helper(sar_rm_imm_v);	/* 0xC1 /7 ok */
 #endif
