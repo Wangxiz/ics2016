f1ff52f3d39f233c3f83058082dac2a81925dc30
tracer-ics2016 <tracer@njuics.org>
2017-04-02 21:52:42 +0800

    >  compile NEMU 141220105 wangxiz Linux wangxiz-virtual-machine 4.4.0-34-generic #53-Ubuntu SMP Wed Jul 27 16:06:28 UTC 2016 i686 i686 i686 GNU/Linux  21:52:42 up 1 day, 20:35,  1 user,  load average: 0.22, 0.23, 0.18 474a569e6086c3bbd65fce1e2f14b74e9622eb5c

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -14,12 +14,17 @@ static void do_execute() {
 	printf("ESP:%x\n", MEM_R(cpu.esp));
 #endif
 
-	if(DATA_BYTE == 4) {
+	if(op_src->type == OP_TYPE_IMM) {
 		cpu.eip += op_src->val;
+		snprintf(op_src->str, OP_STR_SIZE, " 0x%x", cpu.eip + len + 1);
 	}
-	print_asm(str(instr) str(SUFFIX) " 0x%x", cpu.eip + len + 1);
+	else {
+		cpu.eip = op_src->val - len - 1;
+	}
+	print_asm_template1();
 }
 
 make_instr_helper(si)
+make_instr_helper(rm)
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control/call.h
+++ nemu/src/cpu/exec/control/call.h
@@ -1,6 +1,7 @@
 #ifndef __CALL_H__
 #define __CALL_H__
 
-make_helper(call_si_v);
+make_helper(call_si_v);		/* 0xE8 ok */
+make_helper(call_rm_v);		/* 0xFF /2 ok */
 
 #endif
